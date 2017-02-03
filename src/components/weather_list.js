69e14cd1c0e5a42885d85aba6ad99cefd7b8fe7b
Gurpreet Singh <gsran82@gmail.com>
2017-02-03 21:23:56 +0000

    Added charts and tests

--- src/components/weather_list.js
+++ src/components/weather_list.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
+import Chart from './chart';
+import GoogleMap from './google_map';
 
 class WeatherList extends React.Component {
   renderWeather(cityData) {
-    const name = cityData.city.name;
+    const { id, name } = cityData.city;
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273.15);
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
+
       return (
-        <tr key={name}>
-          <td> {name} </td>
+        <tr key={id}>
+          <td>
+            {name}
+            <GoogleMap lon={lon} lat={lat} />
+          </td>
+          <td><Chart data={temps} color="blue" units="c" /></td>
+          <td><Chart data={pressure} color="orange" units="hPa" /></td>
+          <td><Chart data={humidity} color="red" units="%" /></td>
         </tr>
       );
   }
@@ -17,9 +31,9 @@ class WeatherList extends React.Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (c)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,20 @@
-import { renderComponent , expect } from '../test_helper';
+/* global it, describe, beforeEach, expect */
+
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
+describe('App', () => {
   let component;
 
   beforeEach(() => {
     component = renderComponent(App);
   });
 
-  it('renders something', () => {
-    expect(component).to.exist;
+  it('shoud have an input', () => {
+    expect(component.find('input')).to.exist;
+  });
+
+  it('shoud have a button', () => {
+    expect(component.find('button')).to.exist;
   });
 });
